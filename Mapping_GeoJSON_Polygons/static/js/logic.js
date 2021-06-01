7b6836f2831a7b4cdeb0b454f7b9af38d6ec58ad
David Brown <quinsoq@gmail.com>
2021-05-31 21:11:05 -0700

    county lines got nothing on me

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -7,7 +7,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-let satteliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sattelite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -16,13 +16,13 @@ let satteliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satt
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
-  "Sattelite Streets": satteliteStreets
+  "Satelite Streets": satelliteStreets
 };
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [44.0, -80.0],
-  zoom: 2,
-  layers: [satteliteStreets]
+  center: [43.7, -79.3],
+  zoom: 10,
+  layers: [satelliteStreets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -33,7 +33,6 @@ let torontoHoods = "https://raw.githubusercontent.com/qklm/Mapping_Earthquakes/m
 
 d3.json(torontoHoods).then(function(data) {
   console.log(data);
-  L.geoJson(data, {weight: 1, fillColor: 'yellow', color: 'blue', fillOpacity: 0.4
-  }).bindPopup("<h3> Neighborhood: " + feature.properties.area_name + "</h3>")
+  L.geoJson(data)
   .addTo(map);
 })
